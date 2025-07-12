cc7a2aa95b20cbf18a4bc64cbf8cdea417d4523a
Beterraba <ls6182315@gmail.com>
2025-07-12 08:50:10 -0300

    subscribe communities and people

--- client/main.js
+++ client/main.js
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client';
 import { App } from '../ui/App';
 
 Meteor.startup(() => {
+  Meteor.subscribe('communities');
+  Meteor.subscribe('people');
   const root = createRoot(document.getElementById('app'));
   root.render(<App />);
 });
